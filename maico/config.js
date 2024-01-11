b93170de1d1a32b08deb2963531804485a2a86a7
snrau <simeon.rau@visus.uni-stuttgart.de>
2024-01-11 23:32:23 +0100

    reverted

--- maico/config.js
+++
@@ -1,6 +0,0 @@
-export default function get(){
-    return{
-    port: process.env.PORT || 3000,
-    mongoURI: "mongodb+srv://admin:VGGmhAEDZj4D@aicomposition.ssxufuu.mongodb.net/AIComposition?retryWrites=true&w=majority",
-    }
-}
